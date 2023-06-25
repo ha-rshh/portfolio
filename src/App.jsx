112dec8538df8da886467e7d5d795cfe7574d681
Harsh Prajapati <prajapati.captain@gmail.com>
2023-06-25 19:38:48 +0530

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLInks";
+import SocialLinks from "./components/SocialLinks";
 
 const App = () => {
   return (
